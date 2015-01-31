2b12ea686367de92f732eaf7bf0888b44becc64d
Chris Hammersley <chris.hammersley@gmail.com>
2015-01-31 14:23:40 -0800

    Refactored For-Loop on Pizza Creation

--- views/js/main.js
+++ views/js/main.js
@@ -475,11 +475,12 @@ var resizePizzas = function(size) {
 
 window.performance.mark("mark_start_generating"); // collect timing data
 
-// This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
+  // This for-loop actually creates and appends all of the pizzas when the page loads
+  // CH - moving the variable outside the loop
   var pizzasDiv = document.getElementById("randomPizzas");
-  pizzasDiv.appendChild(pizzaElementGenerator(i));
-}
+  for (var i = 2; i < 100; i++) {
+    pizzasDiv.appendChild(pizzaElementGenerator(i));
+  }
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark("mark_end_generating");
